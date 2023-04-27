021b5c93679652cad82d711a4659997f8b723dfe
Otto Fidélis Siqueira <rm87251@estudante.fieb.edu.br>
2023-04-27 08:19:01 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+    int cont = 0;
+    while (cont<10){
+        System.out.println("Contador = "+cont);
+        cont++;
+    }
+    for (int  x=0; x<10;x++){
+        System.out.println("Contador = "+x);
+    }
+
+        
+}
+}
