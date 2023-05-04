90c4f6a45ac2a8fad6e6107e6d7c370abe9700e9
OttoFidelis <ottofidelis@gmail.com>
2023-05-04 09:55:09 -0300

    Aula de introdução OO

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-    int cont = 0;
-    while (cont<10){
-        System.out.println("Contador = "+cont);
-        cont++;
-    }
-    for (int  x=0; x<10;x++){
-        System.out.println("Contador = "+x);
-    }
+        //aula inreodutória de OO
 
-    cont = 1;
-    do{
-        System.out.println("contador = "+ cont);
-        cont++;
     }
-    while(cont<=10);
-
-}
 }
