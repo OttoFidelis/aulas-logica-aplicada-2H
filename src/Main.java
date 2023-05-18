fd48088778be5c8cb0471973274ed85d15c12bcf
OttoFidelis <ottofidelis@gmail.com>
2023-05-18 11:04:38 -0300

    classe= garrafa d´ádua Adão Power Ranger

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,21 @@ public class Main {
     public static void main(String[] args) {
         //aula inreodutória de OO
 
+        //Declaração de objeto
+        Pessoa adao;
+
+        //Instanciação do objeto
+        adao = new Pessoa();
+
+        //Ao mesmo tempo
+        Pessoa qualquer = new Pessoa();
+
+        //Definir formato do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
+        System.out.println(qualquer.falar("oi"));
+
+
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,14 @@
+public class Pessoa {
+
+    //Atributos de Classe
+    public String nome;
+    public String sobrenome;
+
+    //Metodos de classe
+    public void falar(){
+        System.out.println("falei");
+    }
+    public String falar(String texto){
+        return texto;
+    }
+}
